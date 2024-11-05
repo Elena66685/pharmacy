44d34452df96b4ad4e546b065925913962267578
Елена Писарева <pisarelena@yandex.ru>
2024-11-05 15:11:21 +0300

    Аптека

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
-        Medicines medicines = new Medicines("Анальгин");
-        Medicines n = new Medicines("Спазмалгон");
-        Medicines v = new Medicines("Фурацилин");
+        Medicines medicines = new Medicines("Максавит","Анальгин", 100);
+        Medicines n = new Medicines("Фармани", "Спазмалгон", 45);
+        Medicines v = new Medicines("Оптика", "Фурацилин", 897);
+        Medicines f = new Medicines("Центральная", "Афобазол", 83);
         medicines.addMedicine(medicines);
         medicines.addMedicine(n);
         medicines.addMedicine(v);
+        medicines.addMedicine(f);
         medicines.listMedicine();
-        Sales g = new Sales("apt", "ghj", 100, "fgh", 456, 456);
-        System.out.println(g);
+        //Sales g = new Sales("apt", "ghj", 100, "fgh", 456, 456);
+        //System.out.println(g);
+        Medicines.sortByPrice = false;
+        medicines.sor();
 
     }
 }
--- src/Medicines.java
+++ src/Medicines.java
@@ -1,24 +1,46 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
-public class Medicines {
+public class Medicines implements Comparable<Medicines> {
+    private String name;
     private  String nameMedicine;
+    private int priceMedicine;
     private ArrayList<Medicines> medicines = new ArrayList<>();
+    public static boolean sortByPrice=true;
 
-    public Medicines(String nameMedicine) {
+    public Medicines(String name, String nameMedicine, int priceMedicine) {
+        this.name = name;
         this.nameMedicine = nameMedicine;
+        this.priceMedicine = priceMedicine;
     }
 
     public String getNameMedicine() {
         return nameMedicine;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getPriceMedicine() {
+        return priceMedicine;
+    }
+
     public void setNameMedicine(String nameMedicine) {
         this.nameMedicine = nameMedicine;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPriceMedicine(int priceMedicine) {
+        this.priceMedicine = priceMedicine;
+    }
+
     @Override
     public String toString(){
-        return this.nameMedicine;
+        return this.name + " " + this.nameMedicine + " " + this.priceMedicine;
     }
 
     public void addMedicine (Medicines ob) {
@@ -29,4 +51,18 @@ public class Medicines {
     public void listMedicine() {
         System.out.println(medicines);
     }
+
+    @Override
+    public int compareTo(Medicines o){
+        if(sortByPrice)
+            return this.priceMedicine - o.priceMedicine;
+        else
+            return nameMedicine.compareTo(o.nameMedicine);
+    }
+
+    public void sor() {
+        Collections.sort(medicines);
+        System.out.println(medicines);
+    }
+
 }
